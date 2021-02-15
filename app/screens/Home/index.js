e26f678acd17a6627f8a119bd21f2b8a3f60bcb9
wickwires <shawnamyw@gmail.com>
2021-02-15 14:47:25 -0800

    add image more styling

--- app/screens/Home/index.js
+++ app/screens/Home/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, View, Text} from 'react-native';
+import {Button, Image, View, Text} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 
 import Divider from '../../Components/Divider';
@@ -14,6 +14,19 @@ const Home = () => {
       <View style={styles.headerContainer}>
         <Text style={styles.headerText}>Messages</Text>
       </View>
+      <Divider />
+      <View style={styles.messageHeaderContainer}>
+        {/* <Image
+          style={styles.userImage}
+          source={{
+            uri:
+              'https://i.insider.com/5ba15375e361c01c008b5cf7?width=700&format=jpeg&auto=webp',
+          }}
+        /> */}
+        <Text style={styles.messageNameText}>Jane Doe</Text>
+        <Text style={styles.time}>10:00AM</Text>
+      </View>
+      <Text style={styles.messagePreviewText}>Message Preview</Text>
       <Divider />
       <Button
         title="Go to Details"
--- app/screens/Home/style.js
+++ app/screens/Home/style.js
@@ -3,19 +3,47 @@ import {StyleSheet} from 'react-native';
 const styles = StyleSheet.create({
   body: {
     backgroundColor: '#ffffff',
-    flex: 1,
+    // flex: 1,
     justifyContent: 'space-between',
   },
   headerContainer: {
-    flex: 1,
+    // flex: 1,
     flexDirection: 'row',
     alignItems: 'flex-start',
     marginHorizontal: 20,
     paddingTop: 80,
-    paddingBottom: 10,
   },
   headerText: {
     fontSize: 40,
+    paddingBottom: 10,
+  },
+  messageHeaderContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    paddingBottom: 5,
+  },
+  // userImage: {
+  //   width: 80,
+  //   height: 80,
+  //   alignItems: 'flex-start',
+  // },
+  messageNameText: {
+    fontSize: 16,
+    marginHorizontal: 20,
+    alignItems: 'flex-start',
+  },
+  time: {
+    fontSize: 16,
+    color: '#8e8e93',
+    alignItems: 'flex-end',
+    marginHorizontal: 20,
+  },
+  messagePreviewText: {
+    fontSize: 16,
+    color: '#8e8e93',
+    marginHorizontal: 20,
+    paddingBottom: 10,
   },
 });
 
